7aaabb32b8e522235f36baa78a3108d0dc8fecf4
musaburak <musaburak.erdihan@gmail.com>
2022-05-04 17:22:43 +0300

    problem1

--- src/main/java/com/example/helloworld/Application.java
+++ src/main/java/com/example/helloworld/Application.java
@@ -2,11 +2,17 @@ package com.example.helloworld;
 
 public class Application
 {
+    // https://www.w3resource.com/java-exercises/basic/index.php
+
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
-        Problem17 p17 = new Problem17();
-        p17.test();
+
+        Problem1 p1 = new Problem1();
+        p1.solution();
+
+//        Problem17 p17 = new Problem17();
+//        p17.test();
     }
 
 }
---
+++ src/main/java/com/example/helloworld/Problem1.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+public class Problem1
+{
+    public void solution()
+    {
+        System.out.println("Hello");
+        System.out.println("Musa Burak Erdihan");
+    }
+}
