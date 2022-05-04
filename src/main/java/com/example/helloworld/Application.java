3ef0e0c4124d7ae30b1a0f493952aae77e2d49ed
Muhammet Karafazlioglu <muhammetkarafazlioglu@gmail.com>
2022-05-04 16:30:14 +0300

    Initial commit

---
+++ src/main/java/com/example/helloworld/Application.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Problem17 p17 = new Problem17();
+        p17.test();
+    }
+
+}
