d6c68da81aaf61eb513683060c77ccab5136adaa
Muhammet Karafazlioglu <muhammetkarafazlioglu@gmail.com>
2022-05-05 17:16:30 +0300

    Problem 8 It was successfully run on the application java class and had no debug

---
+++ src/main/java/com/example/helloworld/Problem8.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+public class Problem8
+{
+    public void solution8()
+    {
+        System.out.println("M      M     a    m        m  i  ");
+        System.out.println("M M   MM    a a   m m    m m  i  ");
+        System.out.println("M  M M M   aaaaa  m   m m  m  i  ");
+        System.out.println("M   M  M  a     a m    m   m  i  ");
+    }
+}
