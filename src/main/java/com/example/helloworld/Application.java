ccc1da3d812856c2bf18ee9d1618b392a0b2a14e
Muhammet Karafazlioglu <muhammetkarafazlioglu@gmail.com>
2022-05-05 17:29:31 +0300

    Application This was the final main application after 8 exercises. There is a surprise in 8th exercise. You can erase // to run any exercise you want.

--- src/main/java/com/example/helloworld/Application.java
+++ src/main/java/com/example/helloworld/Application.java
@@ -8,20 +8,33 @@ public class Application
     {
         System.out.println("Hello world!");
 
-        Problem1 p1 = new Problem1();
-        p1.solution();
+        //Problem1 p1 = new Problem1();
+        //p1.solution();
 
-        Problem17 p17 = new Problem17();
-        p17.test();
+        //Problem17 p17 = new Problem17();
+        //p17.test();
 
-        Problem2 p2 = new Problem2();
-        p2.solution2();
+        //Problem2 p2 = new Problem2();
+        //p2.solution2();
 
-        Problem3 p3 = new Problem3();
-        p3.solution3();
+        //Problem3 p3 = new Problem3();
+        //p3.solution3();
 
-        Problem4 p4 = new Problem4();
-        p4.solution4();
+        //Problem4 p4 = new Problem4();
+        //p4.solution4();
+
+        // Problem5 p5 = new Problem5();
+        // p5.solution5();
+
+        //Problem6 p6 = new Problem6();
+        //p6.solution6();
+
+        // Problem7 p7 = new Problem7();
+        // p7.solution7();
+
+        Problem8 p8 = new Problem8();
+        p8.solution8();
+
     }
 
 }
