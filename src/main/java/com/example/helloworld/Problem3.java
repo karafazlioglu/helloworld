685698fd312704a082c5d87edc7cdc6e19a9fcc9
Muhammet Karafazlioglu <muhammetkarafazlioglu@gmail.com>
2022-05-05 16:15:56 +0300

    Problem 3 It was successfully run on the application java class

--- src/main/java/com/example/helloworld/Problem3.java
+++ src/main/java/com/example/helloworld/Problem3.java
@@ -2,22 +2,21 @@ package com.example.helloworld;
 
 import java.util.Scanner;
 
-
 public class Problem3
 {
-    public static void main(String[] args)
+    public void solution3()
     {
-            Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner (System.in);
 
-            System.out.println("Enter the first number: ");
-            int num1 = input.nextInt();
+        System.out.println("Enter the first number: ");
+        int num1 = input.nextInt();
 
-            System.out.println("Enter the first number: ");
-            int num2 = input.nextInt();
+        System.out.println("Enter the first number: ");
+        int num2 = input.nextInt();
 
-            int division = num1 / num2;
+        int division = num1 / num2;
 
-            System.out.println("The result is: " + division);
+        System.out.println("The result is: " + division);
     }
 }
 
