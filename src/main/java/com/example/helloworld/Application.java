272158998d8d1e70fbc0f20221c37003697fac18
Muhammet Karafazlioglu <muhammetkarafazlioglu@gmail.com>
2022-05-05 16:13:26 +0300

    This one has the main method

--- src/main/java/com/example/helloworld/Application.java
+++ src/main/java/com/example/helloworld/Application.java
@@ -11,8 +11,17 @@ public class Application
         Problem1 p1 = new Problem1();
         p1.solution();
 
-//        Problem17 p17 = new Problem17();
-//        p17.test();
+        Problem17 p17 = new Problem17();
+        p17.test();
+
+        Problem2 p2 = new Problem2();
+        p2.solution2();
+
+        Problem3 p3 = new Problem3();
+        p3.solution3();
+
+        Problem4 p4 = new Problem4();
+        p4.solution4();
     }
 
 }
