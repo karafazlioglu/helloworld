19058b1b9a8f6b022468059c2ab5f15a32976c5b
Muhammet Karafazlioglu <muhammetkarafazlioglu@gmail.com>
2022-05-07 15:15:15 +0300

    ---Problem9--- Successfully debugged and run.

---
+++ src/main/java/com/example/helloworld/Problem9.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+public class Problem9
+{
+    public static void solution9()
+    {
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+}
