f634ed6f603de3f070368d05b8ba568f4032db2b
Muhammet Karafazlioglu <muhammetkarafazlioglu@gmail.com>
2022-05-13 16:11:33 +0300

    Problem 12: while to for fixed

--- src/main/java/com/example/helloworld/Problem12.java
+++ src/main/java/com/example/helloworld/Problem12.java
@@ -7,17 +7,15 @@ public class Problem12
     public static void solution12()
     {
         double sum = 0;
-        double x = 1;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Input the count of numbers (n) you want to calculate the average: ");
         int n = sc.nextInt();
 
-        // TODO for
-        while (x <= n) {
-            System.out.println("Input number " + "("+ (int) x +")" + ":");
+        for(int i = 1; i <= n; i++)
+        {
+            System.out.println("Input number " + "("+ (int) i +")" + ":");
             sum += sc.nextInt();
-            x += 1;
         }
 
         double avgn = (sum / n);
