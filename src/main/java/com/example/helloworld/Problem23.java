720724b1f4e3c140986ac2cd11be134b3e0013eb
musaburak <musaburak.erdihan@gmail.com>
2022-05-20 16:18:06 +0300

    Problem 23 reviewed

--- src/main/java/com/example/helloworld/Problem23.java
+++ src/main/java/com/example/helloworld/Problem23.java
@@ -8,40 +8,27 @@ public class Problem23
     public void solution23()
     {
         Scanner sc = new Scanner(in);
-
         int binaryNumber;
-        int remainder;
-        int sum = 0;
-        int sum2 = 0;
-        int remainder2;
-        int i, l;
-        int j = 1;
-        int k = 1;
-
-        char[] hexaDecimal = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        // TODO binary to hex
         System.out.print("Input a binary number: ");
         binaryNumber = sc.nextInt();
-        int binaryNumber2 = binaryNumber / 10000;
 
-        for (i = 0; i < 4; i ++)
+        char[] hexDecimal = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        StringBuilder hex = new StringBuilder();
+        while(binaryNumber != 0)
         {
-            remainder = binaryNumber % 10;
-            sum = sum + remainder * k;
-            binaryNumber = binaryNumber / 10;
-            k = 2 * k;
+            int sum = 0;
+            int k = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                int remainder = binaryNumber % 10;
+                sum = sum + remainder * k;
+                binaryNumber = binaryNumber / 10;
+                k = 2 * k;
+            }
+            hex.insert(0, hexDecimal[sum]);
+            System.out.println(sum);
         }
 
-        System.out.println(sum);
-
-        for (l = 0; l < 4; l ++)
-        {
-            remainder2 = binaryNumber2 % 10;
-            sum2 = sum2 + remainder2 * j;
-            binaryNumber2 = binaryNumber2 / 10;
-            j = 2 * j;
-        }
-        System.out.println(sum2);
-        System.out.println("Hexadecimal number is " + hexaDecimal[sum2] + hexaDecimal[sum]);
+        System.out.println("Hexadecimal number is " + hex);
     }
 }
