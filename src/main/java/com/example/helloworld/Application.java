73d155748c7d63fe386af55f9b21322ada95a01b
Muhammet Karafazlioglu <muhammetkarafazlioglu@gmail.com>
2022-08-06 16:52:15 +0300

    2 tests and array rearrangement done

--- src/main/java/com/example/helloworld/Application.java
+++ src/main/java/com/example/helloworld/Application.java
@@ -10,7 +10,8 @@ public class Application
     public static void main(String[] args)
     {
 
-        Sort.sortNumbers2();
+        //Sort.sortNumbers2();
+        Sort.sortNumbers();
 
 
        // System.out.println("Hello world!");
--- src/main/java/com/example/helloworld/Sort.java
+++ src/main/java/com/example/helloworld/Sort.java
@@ -6,14 +6,30 @@ public class Sort
 {
     public static void sortNumbers()
     {
-        int[] numbers = {10, 2, 2548, 1, 256, 89, 7, 95, 358, 32};
+        int[] randomNumbers = {10, 2, 2548, 1, 256, 89, 7, 95, 358, 32};
+        int[] smallerToGrater = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] greaterToSmaller = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-        for (int i = 0; i < numbers.length; i++)
+        boolean allSorted = false;
+        int loopCount = 0;
+        while (!allSorted)
         {
-            if (numbers[i] > numbers[i+1])
-                numbers[i] = numbers[i + 1];
+            allSorted = true;
+            for (int i = 0; i < greaterToSmaller.length - 1; i++)
+            {
+                if (greaterToSmaller[i] < greaterToSmaller[i+1])
+                {
+                    allSorted = false;
+                    int num = greaterToSmaller[i];
+                    greaterToSmaller[i] = greaterToSmaller[i + 1];
+                    greaterToSmaller[i + 1] = num;
+                    for (int number : greaterToSmaller) System.out.print(number + " ");
+                    System.out.println(" ");
+                    loopCount += 1;
+                }
+            }
         }
-        System.out.println(numbers);
+        System.out.println("I sorted this array greater to smaller by looping " + loopCount + " times");
     }
     public static void sortNumbers2()
     {
@@ -28,6 +44,7 @@ public class Sort
         int num;
         boolean allSorted = false;
         int tour = 0;
+
         while (!allSorted)
         {
             allSorted = true;
