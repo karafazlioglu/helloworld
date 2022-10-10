a13b86d3f73e0cb5a6073539c16c3daa76c54de2
muhammet <muhammetkarafazlioglu@gmail.com>
2022-10-10 21:43:56 +0300

    Problem 43 was written

---
+++ src/main/java/com/example/helloworld/problems/Problem43.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.problems;
+
+import com.example.helloworld.Problem;
+
+public class Problem43 implements Problem {
+    public void solution()
+    {
+        System.out.println("Twinkle, twinkle, little star,\n" +
+                "\tHow I wonder what you are! \n" +
+                "\t\tUp above the world so high,\n" +
+                "\t\tLike a diamond in the sky.\n" +
+                "Twinkle, twinkle, little star, \n" +
+                "\tHow I wonder what you are");
+    }
+}
