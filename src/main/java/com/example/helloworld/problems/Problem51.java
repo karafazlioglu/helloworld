cc09fbdccd78026c9151d1f2df3e70a9f42aedad
muhammet <muhammetkarafazlioglu@gmail.com>
2023-03-19 10:47:16 +0300

    Problem 51 rearranged

--- src/main/java/com/example/helloworld/problems/Problem51.java
+++ src/main/java/com/example/helloworld/problems/Problem51.java
@@ -13,22 +13,22 @@ public class Problem51 implements Problem
         // String str = sc.nextLine();
 
 
-        String str = "12.499,00 TL";
+        String str = "25";            // str adında stringe sayı ve harften oluşan bir string depolanır.
 
-        int[] intList = new int[6];
+        int[] intList = new int[str.length()];             // intList adında bir integer listesi tanımlanır
 
-        for (int i = 0; i < intList.length; i++)
+        for (int i = 0; i < intList.length; i++)        // intList for döngüsüne eklenir
         {
-            char c = str.charAt(i);
-            int ascii = c;
-            if (ascii < 58 & ascii > 47)
+            char c = str.charAt(i);         // c adında açılan char değişkenine str stringinin karakterleri tanımlanır
+            int ascii = c;                  // c karakterlerinin ascii numarası ascii olarak int depolanır
+            if (ascii < 58 & ascii > 47)    // sadece rakamların listeye dahil edileceği için rakamların ascii numara aralığı if koşulu ile belirlenir
             {
-                intList[i] = (ascii - 48);
+                intList[i] = (ascii - 48);  // string içerisindeki rakamlar tek tek liste içerisine integer olarak dahil edilir.
             }
         }
 
-        for (int j = 0; j < intList.length; j ++)
-            System.out.println(intList[j]);
+        for (int j = 0; j < intList.length; j ++)   // listenin tek satırda sıralı halde gösterimi yapılır.
+             System.out.print(intList[j]);
     }
 
 }
