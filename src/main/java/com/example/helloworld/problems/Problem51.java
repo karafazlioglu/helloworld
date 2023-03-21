c6e09fbc613adb1a6df396fb8b8b2066efa24b08
muhammet <muhammetkarafazlioglu@gmail.com>
2023-03-21 19:21:28 +0300

    Problem 51 comments added

--- src/main/java/com/example/helloworld/problems/Problem51.java
+++ src/main/java/com/example/helloworld/problems/Problem51.java
@@ -19,9 +19,14 @@ public class Problem51 implements Problem
 
             intList[i] = (ascii - 48);  // numbers in the string move to intList that formed before as integer one by one
         }
-
-        for (int j = 0; j < intList.length; j ++)   // The print of the list is done sequentially in a single line
-             System.out.print(intList[j]);
+        int t = 0;          // an integer named t is initialized by 0
+        int n = intList.length;     // an integer named n is initialized by the value of the length of int length list
+        for (int j = 0; j < intList.length; j ++)       // for loop is initialized to sum up numbers in order to their digits
+        {
+            int total = (int) (intList[j] * Math.pow(10 , n - 1));      // an integer called total is put into the for loop to calculate the decimal value of the index
+            n --;           // integer that initialized by int list length is decreased by one for every loop
+            t = total + t;      // integer called t is added to integer total for every loop
+        }
+        System.out.println(t);      // integer t is our last number that represents the integer value of given string
     }
-
 }
