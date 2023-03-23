98b15ac080a20d43f2d22825f22bfb78427c01e4
muhammet <muhammetkarafazlioglu@gmail.com>
2023-03-23 11:06:35 +0300

    Problem50 comments added

--- src/main/java/com/example/helloworld/problems/Problem50.java
+++ src/main/java/com/example/helloworld/problems/Problem50.java
@@ -8,23 +8,23 @@ public class Problem50 implements Problem
 {
     public void solution()
     {
-        int[] nums = new int[300];
-        int n = 0;
-        for (int i = 1; i < 100; i ++)
+        int[] nums = new int[300];          // list named nums is created to be filled later
+        int n = 0;                          // integer named n is initialized to be used in for loop
+        for (int i = 1; i < 100; i ++)      // started from 1 to 100 every integer value will be in the for loop
         {
-            if (i % 3 == 0)
+            if (i % 3 == 0)                 // every value of the list will be divided by 3
             {
-                nums[n] = i;
-                n = n + 1;
+                nums[n] = i;                // if next index can be divided by 3, it will go to the value of index n
+                n = n + 1;                  // for every if clause, n will be one more
             }
         }
 
-        for (int j = 0; j < n; j ++)
-            System.out.print(nums[j] + ", ");
+        for (int j = 0; j < n; j ++)                // another for loop is created to print the whole list
+            System.out.print(nums[j] + ", ");       // comma is put between every element of the list
 
-        System.out.println();
+        System.out.println();                       // a blank line is created to pass to second part of the problem
 
-        int[] nums1 = new int[300];
+        int[] nums1 = new int[300];                 // a new list of nums1 is created for second part
         int m = 0;
 
         for (int l = 1; l < 100; l ++)
